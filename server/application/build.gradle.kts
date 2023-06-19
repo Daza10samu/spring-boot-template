b78b1f687dc1f5abba172572aa0e562e95b32cad
Daza10samu <andreyhigher@gmail.com>
2023-06-19 15:14:12 +0300

    feat: add jobrunr

--- server/application/build.gradle.kts
+++ server/application/build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jobrunr:jobrunr-spring-boot-2-starter:6.1.1")
 
     testFixturesApi(testFixtures(project(":domain")))
 
